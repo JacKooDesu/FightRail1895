390332ea0769fdd205395babb85936f74211ba96
JacKooDesu <kuo741258@gmail.com>
2021-03-25 18:05:19 +0800

    ahhhh

--- Assets/Scripts/Entity/Enemy.cs
+++ Assets/Scripts/Entity/Enemy.cs
@@ -8,11 +8,13 @@ public class Enemy : ScriptableObject
 {
     public string enemyName = "None";
 
+    public int level = 1;
+
     public enum EnemyType   // 種族
     {
         Aboriginal,
         Japanese,
-        Han
+        Hakka
     }
 
     public EnemyType type;
@@ -22,11 +24,13 @@ public class Enemy : ScriptableObject
 
     public float movementSpeed = 5.0f;  // 移動速度
     public float health = 20.0f;    // 血量
-    public float damage = 5.0f;     // 傷害，之後可寫成獨立類別，與種族寫在一起
+    public float damage = 5.0f;     // 基礎傷害，之後可寫成獨立類別，與種族寫在一起
     public float attactSpeed = 1.0f; // 攻擊速度，每秒攻擊次數
 
     public List<Item> dropList = new List<Item>();  // 掉落表
 
+    void DamageCal(){
 
+    }
 
 }
