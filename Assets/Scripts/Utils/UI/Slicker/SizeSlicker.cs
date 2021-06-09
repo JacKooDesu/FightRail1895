b2d9ba22068a3770f0703730af78f606a44bf191
JacKooDesu <kuo741258@gmail.com>
2021-06-10 06:00:17 +0800

    ‧ 新的主選單場景，目前血統選擇初排版 ‧ UI - Builder 圖檔移至專案資料夾中 ‧ UI Slicker類別初建構 ‧ SlickSize類別縮放UI物件

---
+++ Assets/Scripts/Utils/UI/Slicker/SizeSlicker.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+namespace JacDev.Utils.UISlicker
+{
+    public class SizeSlicker : SlickerBase
+    {
+        public Vector2 origin;
+        public Vector2 final;
+        public float speed = 5f;
+
+        private void OnEnable()
+        {
+            if (origin == Vector2.zero)
+            {
+                origin = rect.sizeDelta;
+            }
+        }
+
+        public override void Slick()
+        {
+            StopAllCoroutines();
+            StartCoroutine(UpdateSize(final));
+        }
+
+        public override void SlickBack()
+        {
+            StopAllCoroutines();
+            StartCoroutine(UpdateSize(origin));
+        }
+
+        IEnumerator UpdateSize(Vector2 target)
+        {
+            while ((rect.sizeDelta - final).magnitude >= .001f)
+            {
+                rect.sizeDelta = iTween.Vector2Update(rect.sizeDelta, target, speed);
+                yield return null;
+            }
+            rect.sizeDelta = target;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/UI/Slicker/SlickerBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JacDev.Utils.UISlicker
+{
+    public class SlickerBase : MonoBehaviour
+    {
+        protected RectTransform rect
+        {
+            get
+            {
+                return this.transform as RectTransform;
+            }
+        }
+
+        public virtual void Slick() { }
+
+        public virtual void SlickBack() { }
+    }
+}
+
