95fe78ef7e0fadad894ac9cb41a3da067c214901
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-07-09 01:08:14 +0800

    一些場景meta檔更新

---
+++ Assets/Scripts/Entity/Projectiles/ProjectileSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JacDev.Entity
+{
+    public class ProjectileSpawner : EntitySpawner
+    {
+        Vector3 direction;
+        public void Launch(Vector3 targetPoint)
+        {
+            direction = (targetPoint - transform.position).normalized;
+            StartCoroutine(Spawn(0));
+        }
+
+        protected override IEnumerator Spawn(int index, Transform parent = null)
+        {
+            SpawnSetting setting = spawnSettings[index];
+            GameObject go = Instantiate(setting.entity.prefab);
+            EntityObject eo = go.GetComponent<EntityObject>();
+            go.transform.position = spawnpoint.position;
+            go.transform.SetParent(parent);
+            GameHandler.Singleton.entities.Add(eo);
+
+            ((ProjectileObject)eo).Launch(direction);
+
+            yield break;
+        }
+    }
+}
