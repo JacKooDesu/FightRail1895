eada97096e84ea13c0e22d2eb5570c52f4fe31d1
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-07-09 21:18:09 +0800

    Projectile刪除，與EntitySetting重疊 修正子彈不會消失的問題

--- Assets/Scripts/Entity/Projectiles/ProjectileObject.cs
+++ Assets/Scripts/Entity/Projectiles/ProjectileObject.cs
@@ -5,7 +5,6 @@ namespace JacDev.Entity
 {
     public class ProjectileObject : EntityObject
     {
-        protected Projectile setting;
         public Vector3 direction;
         public Transform hitObject = default;
         protected Vector3 hitPoint = Vector3.zero;
@@ -24,40 +23,51 @@ namespace JacDev.Entity
         public IEnumerator Flying()
         {
             float time = 0f;
-            while (hitObject == null && time <= setting.maxFlyTime)
+            bool hasHit = false;
+            Projectile setting = (Projectile)entitySetting;
+            while (!hasHit && time <= setting.maxFlyTime)
             {
                 RaycastHit hit;
-                Physics.Raycast(transform.position, direction, out hit, setting.speed);
+                Physics.Raycast(transform.position, direction, out hit, setting.speed * Time.deltaTime);
 
                 if (hit.transform != null)
                 {
                     hitObject = hit.transform;
                     hitPoint = hit.point;
+                    hasHit = true;
                 }
+
+                transform.position += direction * setting.speed * Time.deltaTime;
+
+                time += Time.deltaTime;
 
                 yield return null;
             }
 
             if (hitObject != null)
-                Explosive();
+                StartCoroutine(Explosive());
+            else
+                Destroy(gameObject);
         }
 
         IEnumerator Explosive()
         {
+            Projectile setting = (Projectile)entitySetting;
             if (setting.radius == 0)
             {
                 if (hitObject.GetComponent<EntityObject>() != null)
                     if (hitObject.transform.GetComponent<EntityObject>().entitySetting.entityType == setting.targetEntityType)
                         hitObject.transform.GetComponent<EntityObject>().GetDamage(setting.damage);
 
+                Destroy(gameObject);
                 yield break;
             }
 
-            Transform explosive = Instantiate(setting.explosivePrefab);
+            Transform explosive = Instantiate(setting.explosivePrefab, transform);
             explosive.position = hitObject.position;
             bool hasCalDamage = false;
             float t = 0;
-            while (t <= 1)
+            while (t <= 1f)
             {
                 explosive.localScale = t * setting.radius * Vector3.one;
                 if (!hasCalDamage && t >= .5f)
@@ -68,14 +78,21 @@ namespace JacDev.Entity
                         if (hit.transform.GetComponent<EntityObject>() != null)
                         {
                             if (hit.transform.GetComponent<EntityObject>().entitySetting.entityType == setting.targetEntityType)
+                            {
                                 hit.transform.GetComponent<EntityObject>().GetDamage(setting.damage);
+                                print(hit.transform.name);
+                            }
+
                         }
                     }
+                    hasCalDamage = true;
                 }
-                t += Time.deltaTime;
+                t += Time.deltaTime / setting.explosiveTime;
 
                 yield return null;
             }
+
+            Destroy(gameObject);
         }
     }
 }
