1a75698f0f7fcbe89d4a6af1e360e1c6c93ada95
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-07-14 12:27:33 +0800

    看後續需求，是否要自己寫Editor擴展(針對不同AttackType來顯示Projectile或其他變數)

---
+++ Assets/Editor/EnemyEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using JacDev.Entity;
+[CustomEditor(typeof(Enemy))]
+public class EnemyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var enemy = target as Enemy;
+        if (enemy.attackType != AttackType.Melee)
+            enemy.projectile = EditorGUILayout.ObjectField(enemy.projectile, typeof(Projectile), true) as Projectile;
+
+    }
+}
