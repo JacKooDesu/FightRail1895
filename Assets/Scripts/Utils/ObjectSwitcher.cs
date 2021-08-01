55b67c9a20a0b0a6cf2fff208700c1f87de42f68
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-08-01 15:58:47 +0800

    ShopScene 運鏡與UI切換功能加入

---
+++ Assets/Scripts/Utils/ObjectSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSwitcher : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+    public bool hideAtStart;
+    public int defaultObject = 0;
+
+    private void Start()
+    {
+        if (hideAtStart)
+            Switch(defaultObject);
+
+    }
+
+    public void Switch(int index)
+    {
+        foreach (GameObject g in objects)
+        {
+            if (g != null)
+                g.SetActive(false);
+        }
+
+        objects[index].SetActive(true);
+    }
+}
