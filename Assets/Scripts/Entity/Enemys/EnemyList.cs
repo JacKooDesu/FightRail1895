bf22d54acd4248e4c11ec2ca38198e6f9159dfb2
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-08-17 16:08:52 +0800

    EnemyList新增，後續更新敵方單位較方便

---
+++ Assets/Scripts/Entity/Enemys/EnemyList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JacDev.Entity
+{
+    [System.Serializable, CreateAssetMenu(fileName = "Enemy List", menuName = "JacDev/Enemy/Enemy List", order = 1)]
+    public class EnemyList : ScriptableObject
+    {
+        public List<Enemy> enemies = new List<Enemy>();
+    }
+}
+
