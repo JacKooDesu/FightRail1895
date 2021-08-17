3d8820dead744f26db4d744b52d5533dfe3dfd31
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-08-17 17:23:22 +0800

    同GameSceneUIHandler，偷吃步寫法

---
+++ Assets/Scripts/UI/TitleScene/MainSelections.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JacDev.UI.TitleScene
+{
+    public class MainSelections : MonoBehaviour
+    {
+        public GameObject startGame;
+        public GameObject continueGame;
+        public GameObject setting;
+        public GameObject developers;
+        public GameObject leave;
+    }
+}
---
+++ Assets/Scripts/UI/TitleScene/TitleSceneUIHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using JacDev.Utils;
+using JacDev.Utils.UISlicker;
+
+namespace JacDev.UI.TitleScene
+{
+    public class TitleSceneUIHandler : MonoBehaviour
+    {
+        public MainSelections mainSelections;
+        public GameObject hintUI;
+
+        void Start()
+        {
+            BindEvents();
+        }
+
+        void BindEvents()
+        {
+            hintUI.GetComponent<ColorSlicker>().settings[0].onComplete += () =>
+            {
+                StartCoroutine(EventBinder.FunctionDelay(3.5f, () => { hintUI.GetComponent<ColorSlicker>().SlickBack(); }));
+            };
+
+            EventBinder.Bind(
+                mainSelections.continueGame.GetComponent<EventTrigger>(),
+                EventTriggerType.PointerClick,
+                (data) =>
+                {
+                    if (FileManager.Load("/PlayerData", "/PlayerData"))
+                    {
+                        AsyncSceneLoader.Singleton.LoadScene("GenerateTest new UI");
+                    }
+                    else
+                    {
+                        hintUI.GetComponentInChildren<Text>().text = "尚無存檔資料";
+                        hintUI.GetComponent<ColorSlicker>().Slick("in");
+                    }
+                });
+        }
+    }
+}
+
