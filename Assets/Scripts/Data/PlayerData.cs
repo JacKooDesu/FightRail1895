e2fb6bada1fc490c5655ec0281baa60ad342643d
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-08-17 17:24:44 +0800

    PlayerData寫入擊殺設定及塔解鎖狀態，為避免重複寫入數值，Init() 不應該針對數值寫死

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -14,6 +14,18 @@ namespace JacDev.Data
 
         public List<Item> inventory = new List<Item>();     // 道具
         public List<ModData> modDatas = new List<ModData>();    // Mod 綁定資料
+        [Header("塔解鎖進度")]
+        public List<int> towersGrade = new List<int>(); // 0代表未解鎖
+
+        [Header("擊殺計數")]
+        public int totalKill = 0;
+        public List<int> typeKill = new List<int>();  // 依照種族表紀錄擊殺數量
+
+        public void Init()
+        {
+            typeKill.Capacity = GameHandler.Singleton.enemyList.enemies.Count;
+            towersGrade.Capacity = GameHandler.Singleton.towerList.towers.Count;
+        }
     }
 
 }
