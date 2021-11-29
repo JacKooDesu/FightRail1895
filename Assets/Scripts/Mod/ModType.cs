45684d91acd3e41a7555d9a3b3be52a0f6db54af
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-11-30 00:48:46 +0800

    Update mod types

--- Assets/Scripts/Mod/ModType.cs
+++ Assets/Scripts/Mod/ModType.cs
@@ -4,14 +4,25 @@
     {
         Other = -1,
 
-        Damage = 0,
-        AtkSpeed = 1,
-        Accurate = 2,
+        Tower_Damage = 0,
+        Tower_Overclock = 1,
+        Tower_Range = 2,
+        Tower_CritChance = 3,
 
-        Poison = 3,
-        Burn = 4,
-        Dizzy = 5,
+        Cabin_LossyRate,
+        Cabin_FuelEfficiency,
+        Cabin_CollectRange,
 
-        Capacity = 6
+        Train_Capacity,
+        Train_Health,
+        Train_ReShield,
+        Train_Shield,
+        Train_Speed
+
+        // Poison = 3,
+        // Burn = 4,
+        // Dizzy = 5,
+
+        // Capacity = 6
     }
 }
