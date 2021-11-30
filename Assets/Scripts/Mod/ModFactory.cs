ae6e909d8992d9c90a9ef3f84361a93cedbbe23c
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-11-30 20:12:13 +0800

    修正Mod 設定

--- Assets/Scripts/Mod/ModFactory.cs
+++ Assets/Scripts/Mod/ModFactory.cs
@@ -20,27 +20,27 @@ namespace JacDev.Mod
     [System.Serializable]
     public class ModSetting
     {
-        public ModType modType = ModType.Damage;
+        public ModType modType = ModType.Tower_Damage;
         public float baseValue, max, min;
 
         public string ModToString()
         {
-            switch (modType)
-            {
-                case ModType.Damage: return "傷害";
+            // switch (modType)
+            // {
+            //     case ModType.Damage: return "傷害";
 
-                case ModType.AtkSpeed: return "攻速";
+            //     case ModType.AtkSpeed: return "攻速";
 
-                case ModType.Accurate: return "精準";
+            //     case ModType.Accurate: return "精準";
 
-                case ModType.Poison: return "中毒";
+            //     case ModType.Poison: return "中毒";
 
-                case ModType.Burn: return "燃燒";
+            //     case ModType.Burn: return "燃燒";
 
-                case ModType.Dizzy: return "暈眩";
+            //     case ModType.Dizzy: return "暈眩";
 
-                case ModType.Capacity: return "彈匣";
-            }
+            //     case ModType.Capacity: return "彈匣";
+            // }
 
             return "MOD TYPE ERROR";
         }
