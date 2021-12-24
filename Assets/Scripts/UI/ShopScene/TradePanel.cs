ab0afd932b87e40cfde0ba0565cd846378c58bbe
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-12-24 16:54:46 +0800

    整合Shop Scene

--- Assets/Scripts/UI/ShopScene/TradePanel.cs
+++ Assets/Scripts/UI/ShopScene/TradePanel.cs
@@ -10,25 +10,10 @@ namespace JacDev.UI.ShopScene
 {
     public class TradePanel : MonoBehaviour
     {
-        [Header("側欄")]
-        public Transform sideSelector;
+
 
         void Start()
         {
-            foreach (Transform t in sideSelector)
-            {
-                EventBinder.Bind(
-                    t.GetComponent<EventTrigger>(),
-                    EventTriggerType.PointerEnter,
-                    (data) => AudioHandler.Singleton.PlaySound("hover")
-                );
-
-                EventBinder.Bind(
-                    t.GetComponent<EventTrigger>(),
-                    EventTriggerType.PointerClick,
-                    (data) => AudioHandler.Singleton.PlaySound("select")
-                );
-            }
         }
     }
 }
