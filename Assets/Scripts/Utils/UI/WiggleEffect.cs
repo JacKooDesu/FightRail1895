3bec7f4fc18bcc3f712558d30ff7012f538f94c0
JacDev <kuo741258@gmail.com>
2022-03-17 01:41:55 +0800

    更新Title UI
    更新素材。
    * GameScene Camera Clipping Far -> 150

---
+++ Assets/Scripts/Utils/UI/WiggleEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace JacDev.Utils
+{
+    public class WiggleEffect : MonoBehaviour
+    {
+        public float xAmount, yAmount;
+        public float speed;
+
+        private void Update()
+        {
+            float x = Mathf.PerlinNoise(Time.time, 0) * xAmount;
+            float y = Mathf.PerlinNoise(0, Time.time) * yAmount;
+            transform.localPosition = new Vector3(x, y, 0);
+        }
+    }
+
+}
